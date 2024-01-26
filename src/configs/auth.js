388c4c95663dd9241756e5969bf2c267376121a8
VithorCarlos <vithor.carlos99@hotmail.com>
2024-01-26 20:02:12 -0300

    config

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   jwt: {
-    secret: 'cd42f38f9d97cc21f969b5f03d33d43e04f8f136e768289d2ad6daf5dda527a9',
+    secret: process.env.AUTH_SECRET || "default",
     expiresIn: '7d'
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
 require("express-async-errors");
+require("dotenv/config");
 const runMigrations = require("./database/sqlite/migrations");
 const express = require("express");
 const AppError = require("./utils/appError");
 const uploadConfig = require("./configs/upload");
-const cors = require('cors');
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 runMigrations();
 
-
 app.use(routes);
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use((error, request, response, next) => {
   //erro gerado pelo client
@@ -34,4 +34,8 @@ app.use((error, request, response, next) => {
   });
 });
 
-app.listen(3333, () => console.log("server is runnig at port:" + 3333));
+const PORT = process.env.PORT || 3333
+
+app.listen(3333, () =>
+  console.log("server is runnig at port:" + PORT)
+);
